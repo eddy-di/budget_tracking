896a41899cc50d0b0a03d3af9a1652582ea2acc2
eddy.di <eddy.di.fint@gmail.com>
2023-11-08 11:53:29 +0600

    views change to class based way

--- wallet/urls.py
+++ wallet/urls.py
@@ -7,12 +7,12 @@ app_name = 'wallet'
 
 urlpatterns = [
     # representation of wallet spending
-    path('spending', views.spending_list, name='spending_list'),
+    path('spending', views.SpendingListView.as_view(), name='spending_list'),
     path('spending/<int:year>/<int:month>/<int:day>/<slug:spent>', 
          views.spending_detail, 
          name='spending_detail'),
     # representation of wallet earning
-    path('earning', views.income_list, name='earning_list'),
+    path('earning', views.IncomeListView.as_view(), name='earning_list'),
     path('earning/<int:year>/<int:month>/<int:day>/<slug:earned>',
          views.income_detail, 
          name='earning_detail'),
--- wallet/views.py
+++ wallet/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
+from django.views.generic import ListView
 
 from .models.income import Income
 from .models.spending import Spending
 
 
-def spending_list(request):
-    spending_list = Spending.objects.all()
-    # paginating 5 elements in one page
-    paginator = Paginator(spending_list, 5)
-    page_number = request.GET.get('page', 1)
-    try:
-        spending = paginator.page(page_number)
-    except PageNotAnInteger:
-        spending = paginator.page(1)
-    except EmptyPage:
-        spending = paginator.page(paginator.num_pages)
+class SpendingListView(ListView):
+    """
+    Alternative way of representing the list of spendings
+    """
+    model = Spending
+    context_object_name = 'spending'
+    paginate_by = 5
+    template_name = 'spending/list.html'
 
-    return render(request, 
-                  'spending/list.html',
-                  {'spending': spending})
+# def spending_list(request):
+    # spending_list = Spending.objects.all()
+    # # paginating 5 elements in one page
+    # paginator = Paginator(spending_list, 5)
+    # page_number = request.GET.get('page', 1)
+    # try:
+        # spending = paginator.page(page_number)
+    # except PageNotAnInteger:
+        # spending = paginator.page(1)
+    # except EmptyPage:
+        # spending = paginator.page(paginator.num_pages)
+# 
+    # return render(request, 
+                #   'spending/list.html',
+                #   {'spending': spending})
 
 def spending_detail(request, year, month, day, spent):
     spending = get_object_or_404(Spending,
@@ -34,21 +44,32 @@ def spending_detail(request, year, month, day, spent):
                   'spending/detail.html',
                   {'spending': spending})
 
-def income_list(request):
-    earning_list = Income.objects.all()
-    # paginating 5 elements in one page
-    paginator = Paginator(earning_list, 5)
-    page_number = request.GET.get('page', 1)
-    try:
-        earning = paginator.page(page_number)
-    except PageNotAnInteger:
-        earning = paginator.page(1)
-    except EmptyPage:
-        earning = paginator.page(paginator.num_pages)
 
-    return render(request, 
-                  'income/list.html',
-                  {'earning': earning})
+class IncomeListView(ListView):
+    """
+    Alternative way of representing the list of income
+    """
+    model = Income
+    context_object_name = 'earning'
+    paginate_by = 5
+    template_name = 'income/list.html'
+
+
+# def income_list(request):
+    # earning_list = Income.objects.all()
+    # # paginating 5 elements in one page
+    # paginator = Paginator(earning_list, 5)
+    # page_number = request.GET.get('page', 1)
+    # try:
+        # earning = paginator.page(page_number)
+    # except PageNotAnInteger:
+        # earning = paginator.page(1)
+    # except EmptyPage:
+        # earning = paginator.page(paginator.num_pages)
+# 
+    # return render(request, 
+                #   'income/list.html',
+                #   {'earning': earning})
 
 def income_detail(request, year, month, day, earned):
     earning = get_object_or_404(Income,
