7495fd8e5035dc5175503c54ea589f1efa7411f8
eddy.di <eddy.di.fint@gmail.com>
2023-11-10 15:05:23 +0600

    try to add markdown for the spending, but models have no TextField attributes, that is why it is not needed

--- wallet/templatetags/spending_tags.py
+++ wallet/templatetags/spending_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models.spending import Spending
 from django.db.models import Count
+# from django.utils.safestring import mark_safe
+# import markdown
 
 
 register = template.Library()
@@ -22,3 +24,8 @@ def get_most_commented_spendings(count=5):
     return Spending.objects.annotate(
         total_comments=Count('spending_comment')
     ).order_by('-total_comments')[:count]
+
+
+# @register.filter(name='markdown')
+# def markdown_format(text):
+    # return mark_safe(markdown.markdown(text))
