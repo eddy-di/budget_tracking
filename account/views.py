56336dacec95624ed661d0f26ccce00ef4a86161
eddy.di <eddy.di.fint@gmail.com>
2023-12-05 14:20:01 +0600

    add edit page for profile, link it with main wallets page

--- account/views.py
+++ account/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -43,6 +44,7 @@ def register(request):
             new_user = user_form.save(commit=False) # create new user obj but not commit to db
             new_user.set_password(user_form.cleaned_data['password']) # set password to user obj
             new_user.save() # save user obj in db
+            Profile.objects.create(user=new_user) # create profile of new user
             return render(request, 
                           'account/register_done.html', 
                           {'new_user': new_user})
@@ -52,3 +54,24 @@ def register(request):
     return render(request, 
                   'account/register.html', 
                   {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, 
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, 
+                                       data=request.POST, 
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    
+    return render(request, 
+                  'account/edit.html', 
+                  {'user_form': user_form, 
+                   'profile_form': profile_form})
