c14c7d7af1c3b171838e492a1021969f0d17b4c3
eddy.di <eddy.di.fint@gmail.com>
2023-12-05 13:09:22 +0600

    add o2o profile model to user, add profile model to admin page

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from wallet.sitemaps import SpendingSitemap, IncomeSitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 sitemaps = {
@@ -32,3 +34,8 @@ urlpatterns = [
          name='django.contrib.sitemaps.views.sitemap'),
     # path(),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
