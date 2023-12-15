dd616bb5863e9ea984eebb314d2b95bb0a3901d6
eddy.di <eddy.di.fint@gmail.com>
2023-12-15 16:23:51 +0600

    update logic of counting bar charts data

--- wallet/views/wallet.py
+++ wallet/views/wallet.py
@@ -133,29 +133,14 @@ def filter_by_date(request, wallet_id):
             # end for doungnut
         # for bars
         
-        expenses = filtered_expense_queryset.values_list('amount', 'category__name', 'sub_category__name')
-        incomes = filtered_income_queryset.values_list('amount', 'category__name', 'sub_category__name')
+        expenses = filtered_expense_queryset.values('category__name').annotate(total=Sum('amount'))
+        incomes = filtered_income_queryset.values('sub_category__name').annotate(total=Sum('amount'))
+            
+        data_expenses_category = [ i['total'] for i in list(expenses) ]
+        labels_expenses_category = [i['category__name'] for i in list(expenses)]
 
-        expenses_categories_d = {}
-            # getting expense sums based on categories and summing them by amount
-        for i in list(expenses):
-            if i[1] not in expenses_categories_d:
-                expenses_categories_d[i[1]] = i[0]
-            else:
-                expenses_categories_d[i[1]] += i[0]
-        
-        income_subcategories_d = {}
-        for i in list(incomes):
-            if i[2] not in income_subcategories_d:
-                income_subcategories_d[i[2]] = i[0]
-            else:
-                income_subcategories_d[i[2]] += i[0]
-        
-        data_expenses_category = [ str(v) for v in expenses_categories_d.values() ]
-        labels_expenses_category = [k for k in expenses_categories_d.keys()]
-
-        data_incomes_subcategory = [ str(v) for v in income_subcategories_d.values() ]
-        labels_incomes_subcategory = [k for k in income_subcategories_d.keys()]
+        data_incomes_subcategory = [ i['total'] for i in list(incomes) ]
+        labels_incomes_subcategory = [i['sub_category__name'] for i in list(incomes)]
 
         return JsonResponse({'expense_sum': filtered_expense_sum,
                        'income_sum': filtered_income_sum,
